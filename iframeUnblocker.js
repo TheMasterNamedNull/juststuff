0529bc2671d13b9b01f0bf1eb0612b760bbc7dc4
TheMasterNamedNull <114673223+TheMasterNamedNull@users.noreply.github.com>
2022-11-03 07:57:56 -0400

    Create iframeUnblocker.js

---
+++ iframeUnblocker.js
@@ -0,0 +1,11 @@
+document.body.style.overflow = "none";
+var x = document.createElement("iframe");
+x.src = "https://hore-buttered-reply.glitch.me";
+x.style.border = "0";
+x.style.left = "0";
+x.style.top = "0";
+x.style.width = "100%";
+x.style.height = "100%";
+x.style.zIndex = "10000";
+x.style.position = "absolute";
+document.body.appendChild(x);
